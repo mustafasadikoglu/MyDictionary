daf19d5deb30a403cdc1aec261bef15e20545f79
Mustafa Sadıkoğlu <m.sadikoglu@msn.com>
2021-01-23 13:07:58 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<string> sayilar2 = new MyList<string>();
+            sayilar2.Add("Istanbul");
+            sayilar2.Add("Ankara");
+            Console.WriteLine(sayilar2.Count);
+
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+
+        public MyList()
+        {
+            _array = new T[0];
+        }
+       public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count => _array.Length;
+    }
+}
